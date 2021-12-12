1dae9594385c8fa6a0207b2f11486154cd98610b
bhalemegha <megha.bhale6@gmail.com>
2021-12-11 21:16:30 -0600

    updated hook

--- visual-mode/src/hooks/useVisualMode.js
+++ visual-mode/src/hooks/useVisualMode.js
@@ -1,19 +1,26 @@
-const { useState } = require("react");
+import { useState } from "react";
+export default function useVisualMode(init) {
+  const [mode, setMode] = useState(init);
+  //List of all prev modes is History
+  const [history, setHistory] = useState([init]);
 
-///  Replace this code with your version of useVisualMode
+  //replacing last history element with newmode
+  const transition = function (newMode, replace=false) {
+    if(replace){
+      back();
+    }
+    setHistory(prev => [...history, newMode])
+    setMode(newMode);
+  }
 
-const useVisualMode = function(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
-
-  const transition = function(newMode, replace = false) {
-  };
-
-  const back = function() {
-  };
-
-  // Don't forget this!  history is needed here
-  return { mode, transition, back, history };
-};
-
-export default useVisualMode;
+  const back = function () {
+    //To prevent initial element from deleting
+    if (history.length > 1) {
+      //It will remove top most element from History
+      history.splice(-1);
+      setHistory(history);
+      setMode(history[history.length - 1]);
+    }
+  }
+  return { mode, history, transition, back };
+}
